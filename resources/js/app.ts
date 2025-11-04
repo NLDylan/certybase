b6957a9a7e46cfd1304c48cc1393608972cdcd38
NLDylan <info@blendingmedia.nl>
2025-11-04 10:11:59 +0100

    Added broadcasting Reverb

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,6 +7,11 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { createPinia } from 'pinia';
 import { initializeTheme } from './composables/useAppearance';
+import { configureEcho } from '@laravel/echo-vue';
+
+configureEcho({
+    broadcaster: 'reverb',
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
